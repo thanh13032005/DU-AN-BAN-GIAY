fc68652d12f102453a974c87896bdef588f65834
Thanh <macbook@Thanh.local>
2025-10-17 15:38:00 +0700

    Initial commit

---
+++ src/test/java/org/example/duantotnghiep/DuAnTotNghiepApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.duantotnghiep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuAnTotNghiepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
